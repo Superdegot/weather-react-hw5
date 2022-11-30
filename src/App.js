27e6a23860bfac7a30915e8f3c92e84fc3a84666
Superdegot <65909405+Superdegot@users.noreply.github.com>
2022-11-30 15:44:54 +0000

    Fixed Search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
-import './App.css';
+import React from "react";
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather app</h1>
+        <Weather defaultCity="London" />
+       
         <footer>
           This project was coded by Ekaterina Filatova and is{" "}
           <a
@@ -16,6 +18,7 @@ function App() {
             open-sourced on GitHub
           </a>
         </footer>
+
       </div>
     </div>
   );
