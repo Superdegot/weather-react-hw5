7c8aca508707b8f75a63789bca498169d7f8f205
Superdegot <65909405+Superdegot@users.noreply.github.com>
2022-11-30 11:00:34 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Ekaterina Filatova and is{" "}
-        <a
-          href="https://github.com/Superdegot/weather-react-hw5"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Ekaterina Filatova and is{" "}
+          <a
+            href="https://github.com/Superdegot/weather-react-hw5"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
