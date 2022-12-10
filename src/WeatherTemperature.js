142be9fc15f5815c319b034d499e3686bc33e25f
Superdegot <65909405+Superdegot@users.noreply.github.com>
2022-12-10 11:30:31 +0000

    fixed bugs

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,41 +1,14 @@
-import React, {useState} from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-    const [unit,setUnit] = useState('celsius');
-function convertToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-}
-function covertToCelsius(event) {
-     event.preventDefault();
-     setUnit("celsius");
-}
-
-
-    if (unit === 'celsius') {
-return (
+   return (
   <div className="WeatherTemperature">
     <span className="temperature">{Math.round(props.celsius)}</span>
     <span className="unit">
-      °C | <a href="/" onClick={convertToFahrenheit}>°F </a>
+      °C 
     </span>
   </div>
 );
     }
 
-    else {
-        let fahrenheit = (props.celsius*9)/5+32;
-        return (
-          <div className="WeatherTemperature">
-            <span className="temperature">{Math.round(fahrenheit)}</span>
-            <span className="unit">
-              <a href="/" onClick={covertToCelsius}>°C </a> |{" "}
-              
-                °F{" "}
-            
-            </span>
-          </div>
-        );
-    }
-    
-}
+   
